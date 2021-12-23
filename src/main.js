e5cd94a513e4ee8762c9563b9754fdb8b7d764d5
Victor Hiairrassary <victor.hiairrassary@gmail.com>
2021-12-23 09:02:05 +0100

    wip

--- src/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_s3_1 = require("@aws-sdk/client-s3");
-const s3Client = new client_s3_1.S3Client({});
-s3Client.send(new client_s3_1.PutObjectCommand({ Bucket: 'bucket', Key: 'key', Body: 'body' }));
-//# sourceMappingURL=main.js.map
